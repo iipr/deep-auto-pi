78b923b92768df4ac3be5872ba0e899ee63db24c
Ignacio Iker Prado Rujas <iker.prado13@gmail.com>
2019-08-14 17:22:49 +0000

    Big refactoring: include model name in the object, save_meta function, make use of get_path...

--- deep-playground/deep-playground.py
+++ deep-playground/deep-playground.py
@@ -25,7 +25,7 @@ BATCH_SIZE = 2**5
 SHUFFLE_DATA_GEN = False
 TIMESTEP = 3 # <- for LSTMs
 TRAIN_SPLIT = 0.9
-VAL_SPLIT = 1 - TRAIN SPLIT
+VAL_SPLIT = 1 - TRAIN_SPLIT
 UNSEEN_VID = '/V0420043'
 RNN_NAMES = ['rnn', 'lstm', 'gru']
 
@@ -33,7 +33,7 @@ RNN_NAMES = ['rnn', 'lstm', 'gru']
 #                  TRAINING                     #
 #################################################
 
-def train_sequentially(model, mod_name, n_epochs):
+def train_sequentially(model, n_epochs):
     '''For the case of LSTMs.'''
     # Define parameters for the generator
     params = {'batch_size': 1 if has_rnn_layer(model) else BATCH_SIZE,
@@ -64,7 +64,6 @@ def train_sequentially(model, mod_name, n_epochs):
             # Indexes for train and validation
             train_idx = np.arange(n_samples)[0:int(n_samples * train_split)]
             val_idx = np.arange(n_samples)[int(n_samples * train_split):n_samples]
-
             # Generators
             train_generator = DataGenerator(train_idx, **params)
             val_generator = DataGenerator(val_idx, **params)
@@ -72,13 +71,13 @@ def train_sequentially(model, mod_name, n_epochs):
             model.fit_generator(generator=train_generator,
                                 validation_data=val_generator, shuffle=False,
                                 initial_epoch=epoch, epochs=epoch+1,
-                                callbacks=[HistoryCallback(mod_name=mod_name, video=video,
-                                                           log_file='logs_' + mod_name)],
+                                callbacks=[HistoryCallback(mod_name=model.name, video=video,
+                                                           log_file=get_path(MODEL_PATH, model.name, '_logs.txt'))],
                                 use_multiprocessing=True, workers=2)
             model.reset_states()
-    return model
+    return model, params
 
-def train_shuffled(model, mod_name, n_epochs):
+def train_shuffled(model, n_epochs):
     '''For the case of other NN (like CNNs).'''
     # Define parameters for the generator
     params = {'batch_size': 1 if has_rnn_layer(model) else BATCH_SIZE,
@@ -92,43 +91,43 @@ def train_shuffled(model, mod_name, n_epochs):
                 'group_y': DIST_SHUFFLED_GROUP
                }
              }
-
     # Total number of samples
     with pd.HDFStore(params['files']['file_y'], mode='r') as store_y:
         n_samples = store_y[params['files']['group_y']].shape[0]
     validation_split = VAL_SPLIT
-
     # Indexes of the validation set
     val_idx = np.random.choice(np.arange(n_samples), replace=False,
                                size=int(np.floor(n_samples * validation_split)))
     # Indexes of the training set
     train_idx = np.array(list(set(np.arange(n_samples)) - set(val_idx)))
-
     # Generators
     train_generator = DataGenerator(train_idx, **params)
     val_generator = DataGenerator(val_idx, **params)
 
     model.fit_generator(generator=train_generator,
-                        validation_data=val_generator, #shuffle=False,
-                        epochs=n_epochs, callbacks=[HistoryCallback(mod_name)],
+                        validation_data=val_generator, 
+                        epochs=n_epochs, #shuffle=False,
+                        callbacks=[HistoryCallback(mod_name=model.name, 
+                                                   log_file=get_path(MODEL_PATH, model.name, '_logs.txt'))],
                         use_multiprocessing=True, workers=2)
-    return model
+    return model, params
 
-def train_models(mod_list, mod_names, epochs_list):
+def train_models(mod_list, epochs_list, models):
     for idx, model in enumerate(mod_list):
-        mod_name, n_epochs = mod_names[idx], epochs_list[idx]
-        if not os.path.exists('../data/models/{}/'.format(mod_name)):
-            os.mkdir('../data/models/{}/'.format(mod_name))
-        print('\n\n\t{}. Starting to train model {}...\n'.format(idx, mod_name))
-        if any(sb_str in mod_name for sb_str in ['rnn', 'lstm', 'gru']):
-            model = train_sequentially(model, mod_name, n_epochs)
+        n_epochs = epochs_list[idx]
+        if not os.path.exists('{}{}/'.format(MODEL_PATH, model.name)):
+            os.mkdir('{}{}/'.format(MODEL_PATH, model.name))
+        print('\n\n\t{}. Starting to train model {}...\n'.format(idx, model.name))
+        if has_rnn_layer(model):
+            model, params = train_sequentially(model,n_epochs)
         else:
-            model = train_shuffled(model, mod_name, n_epochs)
-        model.save('../data/models/{}/{}.h5'.format(mod_name, mod_name))
-
+            model, params = train_shuffled(model, n_epochs)
+        model.save(get_path(MODEL_PATH, model.name, '.h5'))
+        # For reproducibility:
+        save_meta(model, n_epochs, params, models)
 
 def prepare_training(models):
-    mod_list, mod_names, epochs_list = [], [], []
+    mod_list, epochs_list = [], []
     while True:
         print('\n\t1. Create new model.')
         print('\t2. Load existing model.')
@@ -142,16 +141,17 @@ def prepare_training(models):
             if mod_abbr in models.get_models_list():
                 model = models.model(mod_abbr)
             else: print('\n\tWrong model!'); continue
-        elif option == 2: model, _ = load_model(models)
-        elif option == 3: print('\n\t{}'.format(list(zip(mod_names, epochs_list)))); continue
-        elif option == 4: train_models(mod_list, mod_names, epochs_list); break
+        elif option == 2: model = load_model(models)
+        elif option == 3: print('\n\t{}'.format([(mod_list[i].name, epochs_list[i]) 
+                                                 for i in range(len(mod_list))])); continue
+        elif option == 4: train_models(mod_list, epochs_list, models); break
         elif option == 5: break
         else:
             print('\n\tInvalid option!')
             input('\n\tPress Enter to continue...')
             continue
+        model.name = input('\n\tPlease enter a new model name: ')
         mod_list.append(model)
-        mod_names.append(input('\n\tPlease enter a model name: '))
         epochs_list.append(int(input('\n\tNumber of epochs: ')))
         if input('\n\tSee model summary? [y/n]: ') == 'y':
             model.summary()
@@ -162,9 +162,9 @@ def prepare_training(models):
 #                  TESTING                      #
 #################################################
 
-def loss_plot(model, mod_name):
+def loss_plot(mod_name):
     # Load history file
-    hist = pd.read_csv('../data/models/{}/{}_hist.csv'.format(mod_name, mod_name))
+    hist = pd.read_csv(get_path(MODEL_PATH, mod_name, '_hist.csv'))
     days = hist['duration [s]'].sum() // (24 * 3600)
     print('\n\tTotal training time for model {} was {} days and {}'.format(mod_name, days,
         time.strftime('%H:%M:%S', time.gmtime(hist['duration [s]'].sum()))))
@@ -184,9 +184,9 @@ def loss_plot(model, mod_name):
     axs[1].set_yscale('log')
     axs[1].grid()
     # Save resulting figure
-    fig.savefig('../data/models/{}/{}_loss.png'.format(mod_name, mod_name))
-    print('\n\tLoss plots saved in ../data/models/{}/{}_loss.png'.format(mod_name, mod_name))
-    return
+    plt_path = get_path(MODEL_PATH, mod_name, '_loss.png')
+    fig.savefig(plt_path)
+    print('\n\tLoss plots saved in {}'.format(plt_path))
 
 def scatter_plot(y_true, y_pred, mod_name, unseen_shuffled=''):
     max_val = max(max(y_true), max(y_pred)) + 5
@@ -200,12 +200,11 @@ def scatter_plot(y_true, y_pred, mod_name, unseen_shuffled=''):
     ax.set_ylabel('Predicted distances', fontsize=12)
     ax.set_title('Scatter plot of true vs predicted distances', fontsize=15)
     # Save resulting figure
-    fig.savefig('../data/models/{}/{}_scatter_{}_{}.png'.format(mod_name, mod_name, unseen_shuffled, length))
-    print('\n\tScatter plot saved in ../data/models/{}/{}_scatter_{}_{}.png'.format(mod_name, mod_name,
-                                                                                     unseen_shuffled, length))
+    plt_path = get_path(MODEL_PATH, mod_name, '_scatter_{}_{}.png'.format(unseen_shuffled, length))
+    fig.savefig(plt_path)
+    print('\n\tScatter plot saved in {}'.format(plt_path))
 
 def series_plot(y_true, y_pred, mod_name):
-    #max_val = max(max(y_true), max(y_pred)) + 5
     length = len(y_true)
     fig, ax = plt.subplots()
     fig.set_size_inches(20, 10)
@@ -218,10 +217,11 @@ def series_plot(y_true, y_pred, mod_name):
     ax.legend(['True', 'Predicted'], fontsize=12)
     ax.set_title('Series plot of true and predicted distances', fontsize=15)
     # Save resulting figure
-    fig.savefig('../data/models/{}/{}_series_{}.png'.format(mod_name, mod_name, length))
-    print('\n\tSeries plot saved in ../data/models/{}/{}_series_{}.png'.format(mod_name, mod_name, length))
+    plt_path = get_path(MODEL_PATH, mod_name, '_series_{}.png'.format(length))
+    fig.savefig(plt_path)
+    print('\n\tSeries plot saved in {}'.format(plt_path))
 
-def test_model_unseen(model, mod_name, n_frames, shuffled):
+def test_model_unseen(model, n_frames, shuffled):
     video = UNSEEN_VID
     # Define parameters for the generator
     params = {'batch_size': 1 if has_rnn_layer(model) else BATCH_SIZE,
@@ -239,7 +239,7 @@ def test_model_unseen(model, mod_name, n_frames, shuffled):
     # Total number of samples
     with pd.HDFStore(params['files']['file_y'], mode='r') as store_y:
         y = store_y[params['files']['group_y']]
-        max_samples = y.shape[0]
+    max_samples = y.shape[0]
     # Possibility to compute distaces for ALL frames
     n_frames = max_samples if n_frames == 0 else n_frames
     if shuffled:
@@ -250,16 +250,17 @@ def test_model_unseen(model, mod_name, n_frames, shuffled):
     pred_generator = DataGenerator(pred_idx, **params)
     y_pred = model.predict_generator(pred_generator, verbose=1,
                                      use_multiprocessing=True, workers=2).ravel()
+    # Adjust y_true to take last n values for the case of LSTMs
     y_true = y.iloc[pred_idx[-len(y_pred):], 1]
     print('\n\tResults on the evaluated frames:')
     print('\tMSE: {}'.format(keras.backend.eval(keras.losses.mean_squared_error(y_true, y_pred))))
     print('\tMAE: {}'.format(keras.backend.eval(keras.losses.mean_absolute_error(y_true, y_pred))))
-    scatter_plot(y_true, y_pred, mod_name, unseen_shuffled='U' + ('S' if shuffled else ''))
+    scatter_plot(y_true, y_pred, model.name, unseen_shuffled='U' + ('S' if shuffled else ''))
     if not shuffled:
         # Plot both time series
-        series_plot(y.iloc[pred_idx[-len(y_pred):], :], y_pred, mod_name)
+        series_plot(y.iloc[pred_idx[-len(y_pred):], :], y_pred, model.name)
 
-def test_model_seen(model, mod_name, n_frames):
+def test_model_seen(model, n_frames):
     # Define parameters for the generator
     params = {'batch_size': 1 if has_rnn_layer(model) else BATCH_SIZE,
               'shuffle': SHUFFLE_DATA_GEN,
@@ -276,37 +277,38 @@ def test_model_seen(model, mod_name, n_frames):
     # Total number of samples
     with pd.HDFStore(params['files']['file_y'], mode='r') as store_y:
         y = store_y[params['files']['group_y']]
-        max_samples = y.shape[0]
+    max_samples = y.shape[0]
     pred_idx = np.random.choice(max_samples, size=n_frames, replace=False)
     # Generators
     pred_generator = DataGenerator(pred_idx, **params)
     y_pred = model.predict_generator(pred_generator, verbose=1,
                                      use_multiprocessing=True, workers=2).ravel()
+    # Adjust y_true to take last n values for the case of LSTMs
     y_true = y.iloc[pred_idx[-len(y_pred):], 1]
     print('\n\tResults on the evaluated frames:')
     print('\tMSE: {}'.format(keras.backend.eval(keras.losses.mean_squared_error(y_true, y_pred))))
     print('\tMAE: {}'.format(keras.backend.eval(keras.losses.mean_absolute_error(y_true, y_pred))))
-    scatter_plot(y_true, y_pred, mod_name, unseen_shuffled='SS')
+    scatter_plot(y_true, y_pred, model.name, unseen_shuffled='SS')
 
 def prepare_test(models):
     while True:
         print('\n\t1. Load existing model.')
         print('\t2. Check model summary.')
         print('\t3. Plot model loss and metrics.')
-        print('\t4. Test on unseen frames (V0420043).')
+        print('\t4. Test on unseen frames ({}).'.format(UNSEEN_VID))
         print('\t5. Test on seen random frames.')
         print('\t6. Back to menu.')
         option = int(input('\n\tPlease enter your option: '))
-        if option == 1:   model, mod_name = load_model(models)
+        if option == 1:   model = load_model(models)
         elif option == 2: model.summary()
-        elif option == 3: loss_plot(model, mod_name)
+        elif option == 3: loss_plot(model.name)
         elif option == 4:
             n_frames = int(input('\n\tNumber of frames to test: '))
             shuffled = int(input('\n\tTest sequentially [1] or shuffled [2]?: ')) != 1
-            test_model_unseen(model, mod_name, n_frames, shuffled)
+            test_model_unseen(model, n_frames, shuffled)
         elif option == 5:
             n_frames = int(input('\n\tNumber of (random) frames to test: '))
-            test_model_seen(model, mod_name, n_frames)
+            test_model_seen(model, n_frames)
         elif option == 6: break
         else:
             print('\n\tInvalid option!')
@@ -325,22 +327,42 @@ def has_rnn_layer(model):
     return False
 
 def get_path(path, name, ext):
-    return path + name + '/' + name + '.' + ext
+    return path + name + '/' + name + ext
 
 def load_model(models):
-    print()
+    mod_list = [m for m in os.listdir(MODEL_PATH) if m[0] != '.' and m not in ['old', 'models.txt']]
+    print('\n\tAvailable models are {}'.format(mod_list))
     while True:
         mod_name = input('\tModel file name [*.h5]: ')
-        path = '{}{}/{}.h5'.format(MODEL_PATH, mod_name, mod_name)
-        if os.path.exists(path): 
-        #    mod_name = path.split('/')[-1][:-3]
+        mod_path = get_path(MODEL_PATH, mod_name, '.h5')
+        if os.path.exists(mod_path):
             break
         else:
             print('\n\tWrong file name! -> {}'.format(path))
     do_compile = input('\n\tCompile model? [y/n]: ')
-    model = models.load_model(path, True if do_compile == 'y' else False)
-    return model, mod_name
-    
+    model = models.load_model(mod_path, True if do_compile == 'y' else False)
+    return model
+
+def save_meta(model, n_epochs, params, models):
+    width = 80
+    with open(MODEL_PATH + 'models.txt', 'a+') as models_txt:
+        models_txt.write('_' * width)
+        models_txt.write('\n\nMODEL {}\n'.format(model.name))
+        models_txt.write('\nn_epochs: {}'.format(n_epochs))
+        models_txt.write('\nTRAIN_SPLIT: {}'.format(TRAIN_SPLIT))
+        models_txt.write('\nbatch_size: {}'.format(params['batch_size']))
+        models_txt.write('\nshuffle: {}'.format(params['shuffle']))
+        models_txt.write('\nX_reshape: {}'.format(params['X_reshape']))
+        models_txt.write('\ntimestep: {}'.format(params['timestep']))
+        models_txt.write('\nfiles: \n    {}\n    {}\n    {}\n    {}'.format(
+            *[item for item in params['files'].items()]))
+        models_txt.write('\noptimizer: {}'.format(models.get_optimizer()))
+        models_txt.write('\nloss: {}'.format(models.get_loss()))
+        models_txt.write('\nmetrics: {}'.format(models.get_metrics()))
+        models_txt.write('\nlr: {}\n\n'.format(models.get_lr()))
+        model.summary(line_length=width, print_fn=lambda x: models_txt.write(x + '\n'))
+        models_txt.write('\n\n')
+
 def set_optimizer_options(models):
     while True:
         os.system('cls' if os.name == 'nt' else 'clear')
@@ -363,7 +385,7 @@ def set_optimizer_options(models):
             continue
     
 def reload_models():
-    reload(models_utils)
+    reload(modelUtils)
     models = modelUtils.Models()
     input('\n\tDone! Press Enter to continue...')
     return models
