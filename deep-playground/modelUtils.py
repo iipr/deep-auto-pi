6d6f8620bbe0e350642be0a210d2e3b929d46be8
Ignacio Iker Prado Rujas <iker.prado13@gmail.com>
2019-08-16 01:25:20 +0000

    Added new some models to test

--- deep-playground/modelUtils.py
+++ deep-playground/modelUtils.py
@@ -1,4 +1,5 @@
 from keras import layers, models, optimizers
+from keras.layers import TimeDistributed
 
 class Models():
     '''
@@ -12,7 +13,8 @@ class Models():
         self.loss = loss
         self.metrics = metrics
         self.lr = lr
-        self.mod_list = ['cnn', 'lstm', 'cnn_lstm']
+        self.mod_list = ['cnn', 'cnn_v1', 'lstm', 'gru',
+                         'cnn_lstm', 'cnn_lstm_v1', 'cnn_gru']
 
     def get_models_list(self):
         return self.mod_list
@@ -20,10 +22,18 @@ class Models():
     def model(self, mod_name):
         if mod_name == 'cnn':
             return self.__cnn()
+        elif mod_name == 'cnn_v1':
+            return self.__cnn_v1()
         elif mod_name == 'lstm':
             return self.__lstm()
+        elif mod_name == 'gru':
+            return self.__gru()
         elif mod_name == 'cnn_lstm':
             return self.__cnn_lstm()
+        elif mod_name == 'cnn_lstm_v1':
+            return self.__cnn_lstm_v1()
+        elif mod_name == 'cnn_gru':
+            return self.__cnn_gru()
         print('Model not found!')
         return None
 
@@ -41,8 +51,8 @@ class Models():
         model = models.Sequential()
         model.add(layers.Conv2D(32, (3, 3), activation='linear',
                                 input_shape=(300, 300, 3)))
-        model.add(layers.LeakyReLU(alpha=0.3))
         model.add(layers.MaxPooling2D((2, 2)))
+        model.add(layers.LeakyReLU(alpha=0.3))
         model.add(layers.Conv2D(64, (3, 3), activation='linear'))
         model.add(layers.MaxPooling2D((2, 2)))
         model.add(layers.LeakyReLU(alpha=0.3))
@@ -53,6 +63,29 @@ class Models():
         model.add(layers.MaxPooling2D((2, 2)))
         model.add(layers.Flatten())
         model.add(layers.Dense(512, activation='linear'))
+        model.add(layers.LeakyReLU(alpha=0.3))
+        model.add(layers.Dense(1, activation='linear'))
+        model.add(layers.LeakyReLU(alpha=0.3))
+        # Compile model and return
+        self.__compile_model(model)
+        return model
+
+    def __cnn_v1(self):
+        model = models.Sequential()
+        model.add(layers.Conv2D(32, (5, 5), activation='linear',
+                              input_shape=(300, 300, 3)))
+        model.add(layers.MaxPooling2D((2, 2)))
+        model.add(layers.LeakyReLU(alpha=0.3))
+        model.add(layers.Conv2D(64, (3, 3), activation='linear'))
+        model.add(layers.MaxPooling2D((2, 2)))
+        model.add(layers.LeakyReLU(alpha=0.3))
+        model.add(layers.Conv2D(128, (4, 4), activation='linear'))
+        model.add(layers.MaxPooling2D((2, 2)))
+        model.add(layers.LeakyReLU(alpha=0.3))
+        model.add(layers.Conv2D(64, (4, 4), activation='relu'))
+        model.add(layers.MaxPooling2D((2, 2)))
+        model.add(layers.Flatten())
+        model.add(layers.Dense(256, activation='linear'))
         model.add(layers.LeakyReLU(alpha=0.3))
         model.add(layers.Dense(1, activation='linear'))
         model.add(layers.LeakyReLU(alpha=0.3))
@@ -77,29 +110,106 @@ class Models():
         self.__compile_model(model)
         return model
 
+    def __gru(self):
+        model = models.Sequential()
+        model.add(layers.recurrent.GRU(units=1, stateful=True, batch_size=50,
+                                        input_shape=(3, 270000),
+                                        dropout=0.3, return_sequences=True))
+        model.add(layers.BatchNormalization())
+        model.add(layers.recurrent.GRU(units=128, stateful=True, dropout=0.3))
+        model.add(layers.BatchNormalization())
+        model.add(layers.Dense(units=512, activation='linear'))
+        model.add(layers.Dropout(0.4))
+        model.add(layers.LeakyReLU(alpha=0.3))
+        model.add(layers.Dense(units=64, activation='linear'))
+        model.add(layers.LeakyReLU(alpha=0.3))
+        model.add(layers.Dense(units=1, activation='linear'))
+        model.add(layers.LeakyReLU(alpha=0.3))
+        # Compile model and return
+        self.__compile_model(model)
+        return model
+
     def __cnn_lstm(self):
         model = models.Sequential()
-     #   model.add(layers.Conv2D(32, (3, 3), activation='linear', batch_size=1,
-     #                      input_shape=(10*300, 300, 3)))
-     #   model.add(layers.LeakyReLU(alpha=0.3))
-     #   model.add(layers.MaxPooling2D((2, 2)))
-     #   model.add(layers.Conv2D(64, (3, 3), activation='linear'))
-     #   model.add(layers.MaxPooling2D((4, 4)))
-     #   model.add(layers.LeakyReLU(alpha=0.3))
-     #   model.add(layers.Reshape(target_shape=(36, 36*64)))
-     #   ##modl1.add(layers.Reshape(target_shape=(22498, 64)))
-     #   model.add(layers.recurrent.LSTM(units=1, stateful=True,
-     #                                   dropout=0.3, return_sequences=True))
-     #   model.add(layers.Reshape(target_shape=(36,1)))
-     #   model.add(layers.recurrent.LSTM(units=128, stateful=True, dropout=0.3))
-     #   model.add(layers.Dense(units=512, activation='linear'))
-     #   model.add(layers.LeakyReLU(alpha=0.3))
-     #   model.add(layers.Dense(units=64, activation='linear'))
-     #   model.add(layers.LeakyReLU(alpha=0.3))
-     #   model.add(layers.Dense(units=1, activation='linear'))
-     #   model.add(layers.LeakyReLU(alpha=0.3))
-     #   # Compile model and return
-     #   self.__compile_model(model)
+        model.add(TimeDistributed(layers.Conv2D(32, (3, 3), activation='linear'), batch_size=1,
+                                  input_shape=(3, 300, 300, 3)))
+        model.add(TimeDistributed(layers.MaxPooling2D((2, 2))))
+        model.add(layers.LeakyReLU(alpha=0.3))
+        model.add(TimeDistributed(layers.Conv2D(64, (3, 3), activation='linear')))
+        model.add(TimeDistributed(layers.MaxPooling2D((4, 4))))
+        model.add(layers.LeakyReLU(alpha=0.3))
+        model.add(layers.Reshape(target_shape=(3, 36*36*64)))
+        model.add(layers.recurrent.LSTM(units=3, stateful=True, dropout=0.3)) # return_sequences=True
+        model.add(layers.Dense(units=128, activation='linear'))
+        model.add(layers.LeakyReLU(alpha=0.3))
+        model.add(layers.Dense(units=512, activation='linear'))
+        model.add(layers.LeakyReLU(alpha=0.3))
+        model.add(layers.Dense(units=64, activation='linear'))
+        model.add(layers.LeakyReLU(alpha=0.3))
+        model.add(layers.Dense(units=1, activation='linear'))
+        model.add(layers.LeakyReLU(alpha=0.3))
+        # Compile model and return
+        self.__compile_model(model)
+        return model
+
+    def __cnn_lstm_v1(self):
+        model = models.Sequential()
+        model.add(TimeDistributed(layers.Conv2D(32, (3, 3), activation='linear'), batch_size=50,
+                                  input_shape=(3, 300, 300, 3)))
+        model.add(TimeDistributed(layers.MaxPooling2D((2, 2))))
+        model.add(layers.LeakyReLU(alpha=0.3))
+        model.add(layers.BatchNormalization())
+        model.add(TimeDistributed(layers.Conv2D(64, (3, 3), activation='linear')))
+        model.add(TimeDistributed(layers.MaxPooling2D((4, 4))))
+        model.add(layers.LeakyReLU(alpha=0.3))
+        model.add(TimeDistributed(layers.Conv2D(16, (3, 3), activation='linear')))
+        model.add(TimeDistributed(layers.MaxPooling2D((4, 4))))
+        model.add(layers.LeakyReLU(alpha=0.3))
+        model.add(layers.Reshape(target_shape=(3, 8*8*16)))
+        model.add(layers.recurrent.LSTM(units=3, stateful=True,
+                                        return_sequences=True, dropout=0.3))
+        model.add(layers.recurrent.LSTM(units=128, dropout=0.3))
+        model.add(layers.LeakyReLU(alpha=0.3))
+        model.add(layers.BatchNormalization())
+        model.add(layers.Dense(units=512, activation='linear'))
+        model.add(layers.Dropout(0.4))
+        model.add(layers.LeakyReLU(alpha=0.3))
+        model.add(layers.Dense(units=64, activation='linear'))
+        model.add(layers.LeakyReLU(alpha=0.3))
+        model.add(layers.Dense(units=1, activation='linear'))
+        model.add(layers.LeakyReLU(alpha=0.3))
+        # Compile model and return
+        self.__compile_model(model)
+        return model
+
+    def __cnn_gru():
+        model = models.Sequential()
+        model.add(TimeDistributed(layers.Conv2D(32, (3, 3), activation='linear'), batch_size=50,
+                                  input_shape=(3, 300, 300, 3)))
+        model.add(TimeDistributed(layers.MaxPooling2D((2, 2))))
+        model.add(layers.LeakyReLU(alpha=0.3))
+        model.add(layers.BatchNormalization())
+        model.add(TimeDistributed(layers.Conv2D(64, (3, 3), activation='linear')))
+        model.add(TimeDistributed(layers.MaxPooling2D((4, 4))))
+        model.add(layers.LeakyReLU(alpha=0.3))
+        model.add(TimeDistributed(layers.Conv2D(16, (3, 3), activation='linear')))
+        model.add(TimeDistributed(layers.MaxPooling2D((4, 4))))
+        model.add(layers.LeakyReLU(alpha=0.3))
+        model.add(layers.Reshape(target_shape=(3, 8*8*16)))
+        model.add(layers.recurrent.GRU(units=3, stateful=True,
+                                        return_sequences=True, dropout=0.3))
+        model.add(layers.recurrent.GRU(units=128, dropout=0.3))
+        model.add(layers.LeakyReLU(alpha=0.3))
+        model.add(layers.BatchNormalization())
+        model.add(layers.Dense(units=512, activation='linear'))
+        model.add(layers.Dropout(0.4))
+        model.add(layers.LeakyReLU(alpha=0.3))
+        model.add(layers.Dense(units=64, activation='linear'))
+        model.add(layers.LeakyReLU(alpha=0.3))
+        model.add(layers.Dense(units=1, activation='linear'))
+        model.add(layers.LeakyReLU(alpha=0.3))
+        # Compile model and return
+        self.__compile_model(model)
         return model
 
     # Optimizer
