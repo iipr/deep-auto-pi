6005b069cbe29be1d4cb64c0c1a70966a9211719
iipr <iker.prado13@gmail.com>
2019-09-09 11:05:26 +0200

    Added comments on how to include the distance inference

--- pi-inference/inference.py
+++ pi-inference/inference.py
@@ -5,18 +5,20 @@ import numpy as np
 
 MODEL_PATH='/home/pi/deep-autopi/data/models/openvino/vehicle_detection/'
 MODEL_NAME='vehicle-detection-adas-0002'
+# MODEL_PATH_1='/home/pi/deep-autopi/data/models/openvino/distance/'
+# MODEL_NAME_1='cnn_v1_ep10'
 OUT_PATH='/home/pi/deep-autopi/data/onroad/'
 PRED_FILE='car_predictions.npy'
 # Maximum time of inference
 MAX_TIME=1
 
-def load_model():
+def load_model(model_path=MODEL_PATH, model_name=MODEL_NAME):
     '''Load and prepare models'''
     #  Plugin initialization for NCS
     plugin = IEPlugin(device="MYRIAD")
     #  Read in Graph file (IR)
-    net = IENetwork(model=MODEL_PATH + MODEL_NAME +'.xml',
-                    weights=MODEL_PATH + MODEL_NAME +'.bin')
+    net = IENetwork(model=model_path + model_name +'.xml',
+                    weights=model_path + model_name +'.bin')
 
     input_shape = net.inputs['data'].shape
     #  Load network to the plugin
@@ -27,6 +29,9 @@ def load_model():
 # Load model
 exec_net, input_shape = load_model()
 n, c, h, w = input_shape
+# # Load model distance
+# exec_net_1, input_shape_1 = load_model(MODEL_PATH_1, MODEL_NAME_1)
+# n_1, c_1, h_1, w_1 = input_shape_1
 # Create folder for outputs
 folder = time.strftime("%Y_%m_%d-%H_%M_%S", time.gmtime())
 out_path = OUT_PATH + folder + '/'
@@ -68,6 +73,14 @@ while(True):
     # Append to npy file
     with open(pred_path, 'ab') as pred_file:
         np.save(pred_file, arr)
+    # if arr[0, 1] > 0.2:
+        # # car detected
+        # frame = frame.reshape((n_1, h_1, w_1, c_1))
+        # frame = cv2.resize(frame, (w_1, h_1))
+        # # Start synchronous inference and get inference result
+        # req_handle_1 = exec_net_1.start_async(0, inputs={'data': frame})
+        # with open(pred_path.replace('.npy', '.csv'), 'ab') as pred_file:
+            # pred_file.write('{},{}'.format(tic, req_handle_1.outputs['detection_out']))
     count += 1
     if time.time() - go > MAX_TIME * 60:
         chkp = ["espeak", "'One minute recorded'"]
